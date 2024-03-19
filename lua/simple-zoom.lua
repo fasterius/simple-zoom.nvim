51152a4adc7250f992d492b9e49801831178ac91
Erik Fasterius <erik.fasterius@outlook.com>
2024-03-19 10:13:17 +0100

    Format with Stylua

--- lua/simple-zoom.lua
+++ lua/simple-zoom.lua
@@ -13,7 +13,7 @@ end
 -- Internal function for zooming in
 local function zoom_in()
     -- Open current split in a new tab
-    vim.cmd[[tab split]]
+    vim.cmd([[tab split]])
 
     -- Optionally hide the tabline
     if M.opts.hide_tabline then
@@ -21,13 +21,13 @@ local function zoom_in()
     end
 
     -- Set tab-specific variable
-    vim.api.nvim_tabpage_set_var(0, 'simple-zoom', 'zoom')
+    vim.api.nvim_tabpage_set_var(0, "simple-zoom", "zoom")
 end
 
 -- Internal function for zooming out
 local function zoom_out()
     -- Store view to get cursor position, folds, etc.
-    vim.cmd[[mkview]]
+    vim.cmd([[mkview]])
 
     -- Show tabline again if it was hidden
     if M.opts.hide_tabline then
@@ -35,27 +35,23 @@ local function zoom_out()
     end
 
     -- Close the tab and return to the un-zoomed view
-    vim.cmd[[tab close]]
+    vim.cmd([[tab close]])
 
     -- Load the stored view
-    vim.cmd[[loadview]]
+    vim.cmd([[loadview]])
 end
 
 -- Main function to toggle zoom state
 function M.toggle_zoom()
     -- Check for tab-specific variable and call the appropriate zoom function
-    if not vim.t['simple-zoom'] then
+    if not vim.t["simple-zoom"] then
         zoom_in()
-    elseif vim.t['simple-zoom'] == 'zoom' then
+    elseif vim.t["simple-zoom"] == "zoom" then
         zoom_out()
     end
 end
 
 -- Add `SimpleZoomToggle` user command
-vim.api.nvim_create_user_command (
-    'SimpleZoomToggle',
-    M.toggle_zoom,
-    { desc = 'Toggle Simple Zoom on and off' }
-)
+vim.api.nvim_create_user_command("SimpleZoomToggle", M.toggle_zoom, { desc = "Toggle Simple Zoom on and off" })
 
 return M
--- plugin/simple-zoom.lua
+++ plugin/simple-zoom.lua
@@ -1,3 +1,3 @@
 if vim.fn.has("nvim-0.7.0") ~= 1 then
-   vim.api.nvim_err_writeln("simple-zoom.nvim requires at least Neovim v0.7.0.")
+    vim.api.nvim_err_writeln("simple-zoom.nvim requires at least Neovim v0.7.0.")
 end
